e14b4f27b9fba5432ef6247bc7254ce7033a2181
Leo Nguyen <Leo.Nguyen@gmx.de>
2022-08-11 18:17:35 +0200

    add bayesflow to PYTHONPATH during git commit and push for pytest to run

--- bin/git_push.sh
+++ bin/git_push.sh
@@ -1,3 +1,4 @@
 # cloud synchronization between multiple machines causes git hooks to not be executable
+export PYTHONPATH=$PYTHONPATH:$(pwd)/ML_for_Battery_Design/external/BayesFlow 
 pre-commit install -t pre-push
 git push

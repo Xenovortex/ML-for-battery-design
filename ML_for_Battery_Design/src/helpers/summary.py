dce0f1280a310832526ea14672b08af3bfcfab74
Leo Nguyen <Leo.Nguyen@gmx.de>
2022-08-12 16:06:55 +0200

    implement LSTM summary network architecture

--- ML_for_Battery_Design/src/helpers/summary.py
+++ ML_for_Battery_Design/src/helpers/summary.py
@@ -1,17 +1,25 @@
+from typing import Type
+
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten
+from bayesflow.default_settings import MetaDictSetting
+from tensorflow.keras.layers import LSTM, Dense, Flatten
 from tensorflow.keras.models import Sequential
 
 
-class FC_net(tf.keras.Model):
+class FC_network(tf.keras.Model):
     """Implements a fully-connected network with keras.
 
     Attributes:
         FC (tf.keras.Model): fully-connected network architecture
     """
 
-    def __init__(self, meta) -> None:
-        super(FC_net, self).__init__()
+    def __init__(self, meta: Type[MetaDictSetting]) -> None:
+        """Initializes :class:FC_network
+
+        Args:
+            meta (bayesflow.MetaDictSetting): contains data to construct network architecture
+        """
+        super(FC_network, self).__init__()
 
         self.FC = Sequential(
             [Flatten()]
@@ -23,10 +31,44 @@ class FC_net(tf.keras.Model):
         """Performs the forward pass of the model
 
         Args:
-            x (tf.Tensor): input data of shape (batch_size, max_time_iter, (nr), num_features)
+            x (tf.Tensor): input data of shape (batch_size, max_time_iter, (nr,) num_features)
 
         Returns:
             out (tf.Tensor): output of shape (batch_size, summary_dim)
         """
         out = self.FC(x)
+        return out
+
+
+class LSTM_network(tf.keras.Model):
+    """Implements a long short-term memory network architecture
+
+    Attributes:
+        TODO
+    """
+
+    def __init__(self, meta: Type[MetaDictSetting]) -> None:
+        """Initializes :class:LSTM_network
+
+        Args:
+            meta (Type[MetaDictSetting]): contains data to construct network architecture
+        """
+        super(LSTM_network, self).__init__()
+
+        self.LSTM = Sequential(
+            [LSTM(unit, return_sequences=True) for unit in meta["units"][:-1]]
+            + [LSTM(meta["units"][-1], return_sequences=False)]
+            + [Dense(meta["summary_dim"], activation="sigmoid")]
+        )
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        """Performs the forward pass of the model
+
+        Args:
+            x (tf.Tensor): input data of shape (batch_size, max_time_iter, (nr,) num_features)
+
+        Returns:
+            out (tf.Tensor): output of shape (batch_size, summary_dim)
+        """
+        out = self.LSTM(x)
         return out

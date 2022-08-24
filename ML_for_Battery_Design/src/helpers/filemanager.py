09ef4762b78e3281623778b4d948becb3fb0e83a
Xenovortex <Leo.Nguyen@gmx.de>
2022-08-24 09:42:27 +0200

    add docstrings to filemanager class

--- ML_for_Battery_Design/src/helpers/filemanager.py
+++ ML_for_Battery_Design/src/helpers/filemanager.py
@@ -2,7 +2,23 @@ import os
 
 
 class FileManager:
+    """Managing file saving path structure based on file type (data, model, result)
+
+    Attributes:
+        mode (str): main file execution mode (Options: train_online, train_offline, generate_data, analyze_sim, evaluate)
+        sim_model_name (str): name of simulation model
+        summary_net_name (str): name of summary network
+        data_name (str): name of dataset
+        filename (str): filename provided by user from main user interface
+    """
+
     def __init__(self, mode: str, **kwargs: str) -> None:
+        """Initializes :class:FileManager
+
+        Args:
+            mode (str): main file execution mode (Options: train_online, train_offline, generate_data, analyze_sim, evaluate)
+            **kwargs (dict): keyword arguments from docopt user interface
+        """
         self.mode = mode
         self.sim_model_name = kwargs["<sim_model>"]
         self.summary_net_name = kwargs["<summary_net>"]
@@ -10,6 +26,14 @@ class FileManager:
         self.filename = kwargs["<filename>"]
 
     def __call__(self, file_type: str) -> str:
+        """Generate save file path based on file type (data, model, result)
+
+        Args:
+            file_type (str): type of file to generate a save path for. Options: data, model, result
+
+        Returns:
+            path (str): save path for given file type
+        """
         if file_type == "data":
             path = os.path.join("data", self.sim_model_name, self.data_name)
         elif file_type == "model":
